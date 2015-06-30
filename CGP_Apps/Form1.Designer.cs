c4b389c559d29eb497044b1a5fc481b54c1d6b1a
mizangit <mizan_diu89@yahoo.com>
2015-06-30 13:24:00 +0600

    SimpleGradeCalculator 1st Upload

---
+++ CGP_Apps/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CGP_Apps
+{
+    partial class CGP_UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.enterMarkTextBox = new System.Windows.Forms.TextBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.enterMarkLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.showButton);
+            this.groupBox1.Controls.Add(this.enterMarkTextBox);
+            this.groupBox1.Controls.Add(this.resultLabel);
+            this.groupBox1.Controls.Add(this.gradeLabel);
+            this.groupBox1.Controls.Add(this.enterMarkLabel);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 205);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result";
+            // 
+            // showButton
+            // 
+            this.showButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showButton.Location = new System.Drawing.Point(259, 37);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 4;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // enterMarkTextBox
+            // 
+            this.enterMarkTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterMarkTextBox.Location = new System.Drawing.Point(134, 37);
+            this.enterMarkTextBox.Name = "enterMarkTextBox";
+            this.enterMarkTextBox.Size = new System.Drawing.Size(100, 26);
+            this.enterMarkTextBox.TabIndex = 3;
+            this.enterMarkTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.enterMarkTextBox_KeyPress);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(153, 106);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 25);
+            this.resultLabel.TabIndex = 2;
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeLabel.Location = new System.Drawing.Point(32, 114);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(53, 17);
+            this.gradeLabel.TabIndex = 1;
+            this.gradeLabel.Text = "Grade";
+            // 
+            // enterMarkLabel
+            // 
+            this.enterMarkLabel.AutoSize = true;
+            this.enterMarkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterMarkLabel.Location = new System.Drawing.Point(29, 41);
+            this.enterMarkLabel.Name = "enterMarkLabel";
+            this.enterMarkLabel.Size = new System.Drawing.Size(87, 17);
+            this.enterMarkLabel.TabIndex = 0;
+            this.enterMarkLabel.Text = "Enter Mark";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 68);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "                  Feature:\r\nThis is a simple Grade calculator.\r\nAll validation ch" +
+    "eck try to fulfill.\r\nEnter key active in text box.";
+            // 
+            // CGP_UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(387, 311);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "CGP_UI";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CGP UI";
+            this.Load += new System.EventHandler(this.CGP_UI_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.TextBox enterMarkTextBox;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label gradeLabel;
+        private System.Windows.Forms.Label enterMarkLabel;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ CGP_Apps/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CGP_Apps
+{
+    public partial class CGP_UI : Form
+    {
+       
+        public CGP_UI()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            Result();
+        }
+
+        private void enterMarkTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                Result();
+            }
+          
+        }
+        public void Result()
+        {
+            try
+            {
+                if (enterMarkTextBox.Text == String.Empty)
+                {
+                    resultLabel.Text = String.Empty;
+                    enterMarkTextBox.BackColor = Color.Red;
+                    MessageBox.Show("Enter mark");
+                    enterMarkTextBox.BackColor = Color.White;
+                }
+                else
+                {
+                    enterMarkTextBox.BackColor = Color.White;
+                    double mark = double.Parse(enterMarkTextBox.Text);
+
+                    if (mark>=0 && mark < 40)
+                    {
+                        resultLabel.ForeColor = Color.Red;
+                        resultLabel.Text = "F";
+
+
+                    }
+                    else if (mark == 40)
+                    {
+                        resultLabel.ForeColor = Color.Green;
+                        resultLabel.Text = "D";
+                    }
+                    else if (mark > 40 && mark <= 50)
+                    {
+                        resultLabel.ForeColor = Color.Green;
+                        resultLabel.Text = "B";
+                    }
+                    else if (mark > 50 && mark <= 60)
+                    {
+                        resultLabel.ForeColor = Color.Green;
+                        resultLabel.Text = "B+";
+                    }
+                    else if (mark > 60 && mark <= 70)
+                    {
+                        resultLabel.ForeColor = Color.Green;
+                        resultLabel.Text = "A-";
+                    }
+                    else if (mark > 70 && mark <= 80)
+                    {
+                        resultLabel.ForeColor = Color.Green;
+                        resultLabel.Text = "A";
+                    }
+                    else if (mark > 80 && mark <= 100)
+                    {
+                        resultLabel.ForeColor = Color.Green;
+                        resultLabel.Text = "A+";
+                    }
+                    else if (mark > 100|| mark<0)
+                    {
+                        enterMarkTextBox.BackColor = Color.Red;
+                        resultLabel.ForeColor = Color.Red;
+                        resultLabel.Text = "Invalid Data!!!!!";
+                        MessageBox.Show("Enter marks between 0 to 100");
+                        enterMarkTextBox.BackColor = Color.White;
+                        enterMarkTextBox.Text = String.Empty;
+                        resultLabel.Text = String.Empty;
+                    }
+
+                }
+            }
+            catch (Exception error)
+            {
+                enterMarkTextBox.BackColor = Color.Red;
+                MessageBox.Show("Enter valid Data");
+                enterMarkTextBox.Text = String.Empty;
+                enterMarkTextBox.BackColor = Color.White;
+            }
+
+        }
+
+        private void CGP_UI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+     
+
+       
+    }
+}
